6f7e499fc8720c636ec5f915ad956de209ca410c
Sowa705 <sowa705@gmail.com>
2019-05-19 13:45:47 +0200

    Added power graph

--- Power Monitor/MainWindow.xaml.cs
+++ Power Monitor/MainWindow.xaml.cs
@@ -11,7 +11,7 @@ using System.Windows;
 using System.Windows.Interop;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-
+using System.Windows.Shapes;
 
 namespace Power_Monitor
 {
@@ -23,6 +23,8 @@ namespace Power_Monitor
         Battery battery;
         public float[] ChargeRate = new float[100];
         public float[] CurrentCharge = new float[100];
+        Line[] lines;
+        int gindex=0;
         public MainWindow()
         {
             InitializeComponent();
@@ -32,6 +34,7 @@ namespace Power_Monitor
 
         public async Task UpdateTask()
         {
+            CreateLines();
             while (true)
             {
                 battery.Update();
@@ -116,15 +119,108 @@ namespace Power_Monitor
 
         public void GraphUpdate()
         {
-            for (int i = 0; i < 99; i++)
+            gindex++;
+            if (gindex>=2)
             {
-                ChargeRate[i + 1] = ChargeRate[i];
+                gindex = 0;
+                Array.Copy(ChargeRate, 1, ChargeRate, 0, ChargeRate.Length - 1);
+                Array.Copy(CurrentCharge, 1, CurrentCharge, 0, ChargeRate.Length - 1);
+                ChargeRate[99] = battery.ChargeRate;
+                CurrentCharge[99] = battery.ChargePercent;
+                DrawGraph();
             }
-            ChargeRate[0] = battery.ChargeRate;
+        }
+        void CreateLines()
+        {
+            lines = new Line[99];
+            for (int i = 1; i < 100; i++)
+            {
+                double w1 = GraphRect.ActualWidth / (i - 1);
+                if (i - 1 == 0)
+                {
+                    w1 = 0;
+                }
+                double h1 = CurrentCharge[i - 1];
+                double w2 = GraphRect.ActualWidth / i;
+                double h2 = CurrentCharge[i];
+                Line l = new Line();
+                l.X1 = w1;
+                l.X2 = w2;
+                l.Y1 = h1;
+                l.Y2 = h2;
+                l.StrokeThickness = 3;
+                l.Stroke = new SolidColorBrush(System.Windows.Media.Color.FromRgb(255, (byte)(127/i), 0));
+                lines[i-1] = l;
+                MainGrid.Children.Add(l);
+            }
         }
         public void DrawGraph()
         {
+            float mi = Math.Abs(CurrentCharge.Min());
+            float mx = Math.Abs(CurrentCharge.Max());
+            if (GraphTypeCombo.SelectedIndex==1)
+            {
+                mi = Math.Abs(ChargeRate.Min());
+                mx = Math.Abs(ChargeRate.Max());
+            }
             
+            float h = 1;
+            if (mi>mx)
+            {
+                h = mi+1;
+            }
+            else
+            {
+                h = mx + 1;
+            }
+            
+            for (int i = 1; i < 100; i++)
+            {
+                double xd = GraphRect.ActualWidth;
+                double w1 = 25+ (i-1) * xd / 100f;
+                double w2 = 25+ i * xd / 100f;
+                double h1 = MainGrid.ActualHeight - 60 - Math.Abs(CurrentCharge[i - 1]) / h * GraphRect.ActualHeight;
+                double h2 = MainGrid.ActualHeight - 60 - Math.Abs(CurrentCharge[i]) / h * GraphRect.ActualHeight;
+                if (GraphTypeCombo.SelectedIndex == 1)
+                {
+                    h1 = MainGrid.ActualHeight - 60 - Math.Abs(ChargeRate[i - 1]) / h * GraphRect.ActualHeight;
+                    h2 = MainGrid.ActualHeight - 60 - Math.Abs(ChargeRate[i]) / h * GraphRect.ActualHeight;
+                }
+                
+                
+                lines[i - 1].X1 = w1;
+                lines[i - 1].X2 = w2;
+                lines[i - 1].Y1 = h1;
+                lines[i - 1].Y2 = h2;
+
+                
+                if (ChargeRate[i-1] > 0)
+                {
+                    lines[i - 1].Stroke = new SolidColorBrush(System.Windows.Media.Color.FromRgb(0, (byte)(127* i/100f), 255));
+                }
+                else
+                {
+                    lines[i - 1].Stroke = new SolidColorBrush(System.Windows.Media.Color.FromRgb(255, (byte)(127*i/100f), 0));
+                }
+                if (ChargeRate[i-1] == 0)
+                {
+                    lines[i - 1].Stroke = new SolidColorBrush(System.Windows.Media.Color.FromArgb(0, 0, 0, 0));
+                }
+                if (ChargeRate[i] == 0)
+                {
+                    lines[i - 1].Stroke = new SolidColorBrush(System.Windows.Media.Color.FromArgb(0, 0, 0, 0));
+                }
+            }
+        }
+
+        private void WindowSize(object sender, SizeChangedEventArgs e)
+        {
+            DrawGraph();
+        }
+
+        private void GraphTypeCombo_SelectionChanged(object sender, EventArgs e)
+        {
+            DrawGraph();
         }
     }
 }
